b9a4792959907c545b1ac242f517262815838637
GgJared <jaredgong13@gmail.com>
2025-11-15 13:40:16 -0800

    Grid Create Function

--- game/game.py
+++ game/game.py
@@ -1,39 +1,94 @@
 
-tiles = [
-    '0':0  #empty Space
-    '#':1  #wall
-    ' ':2  #basic floor
-    "=#":#9  #door, disappears if they have the key for it
-    "<#":#8  #keycard for the door
-    "?#":17  #special interactable, do a function call
-    "E#":#0  #enemy type
-    "^#":#11  #staircase up
-    "v#":#12  #staircase down 
-    "@#":#13  #starts from staircases
-    "c#":#21
-    "p#":#22
+basic_tiles = { #ints are only used for the special code
+    "-":[0,-2],  #empty Space
+    "#":[1,-2],  #wall
+    " ":[2,-2],  #basic floor
+    "=":[9,-1],  #door, disappears if they have the key for it
+    "<":[8,-1],  #keycard for the door
+    "?":[7,-1],  #special interactable, do a function call
+    "E":[0,-1],  #enemy type
+    "^":[11,-1],  #staircase up
+    "v":[12,-1],  #staircase down
+    "@":[13,-1],  #starts from staircases
+    "c":[21,-1],  #chest, refers to chest table
+    "p":[22,-1]  #powerup
+}
+"""
+Used to change the second tuple value:
+tile_type = tile[0]
+tile_number = int(tile[1:])
+"""
 
-]
-chestTable =
-[
-"M250,HP10,MHP10",
-]
+def decode_tiles(line):
+    pattern = r".\d*"     # one character, followed by zero or more digits
+    return re.findall(pattern, line.strip())
 
-print("Start")
-grid = []
+def make_grid(levelFile):
 
-width = 15
-height = 5
+    print("Start")
+    grid = []
+    valueGrid = []
+    chestTable =[#((value it changes, how much it changes by),(...)...)
+    ]
 
-for x in range(width):
-    tmpgrid = []
+    width = 0
+    height = 0
+    lineNum = 0
+    with open(levelFile, "r") as f:
+        txtLine = ""
+        txtList = []
+        width = f[0].strip()
+        height = f[1].strip()
+        for line in f:
+            if (lineNum < height-2):
+                txtLine = line.strip()
+                grid.append(decode_tiles(txtLine))
+            if (lineNum >= height-2):
+                txtLine = line.strip()
+                chestTable.append(decode_tiles(txtLine))
+            lineNum += 1
+    """
+    #make grid
     for y in range(height):
-        tmpgrid.append('0')
-    grid.append(tmpgrid)
+        tmpgrid = []
+        for x in range(width):
+            if(x == 0 or x == width-1 or y == 0 or y == height-2):
+                tmpgrid.append("#")
+            else:
+                tmpgrid.append("-")
+        grid.append(tmpgrid)
+    """
 
-for y in range(height):
+    test_row = ["p21","c9","^1","v1","@1","E1","?1","<1","=1"]
     for x in range(width):
-        print(grid[x][y], end = '')
-        if (y == height-1):
+        #print(x,len(test_row))
+        if x < len(test_row):
+            grid[height-1][x] = test_row[x]
 
-    print("y" + str(y))
+    #print grid
+    for y in range(height):
+        for x in range(width):
+            print(grid[y][x], end = '')
+        print("y" + str(y))
+
+    for y in range(height):
+        tmpgrid = []
+        for x in range(width): 
+            tile = grid[y][x]
+            key = tile[0]
+            if key not in basic_tiles:
+                tmpgrid.append((-1,-1))
+                continue
+            tile_values = basic_tiles[tile[0]]
+            if (tile_values[1] == -1):
+                tile_values[1] = int(tile[1:])
+            tmpgrid.append(tile_values)
+        valueGrid.append(tmpgrid)
+
+    for y in range(height):
+        print(valueGrid[y])
+
+    return(width,height,valueGrid,chestTable)
+
+
+make_grid("level_0.txt")
